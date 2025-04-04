f6a93a782a42a20af5e0f0b96fe01cd69ff34853
Administrator <Administrator@192.168.76.1>
2025-04-04 20:17:03 +0530

    change in code

--- ProjectNinzaCRM/src/test/java/Basic/CreateCampaignTestUsingPropertyFile.java
+++ ProjectNinzaCRM/src/test/java/Basic/CreateCampaignTestUsingPropertyFile.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Properties;
 import java.util.Random;
 
@@ -18,7 +19,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import java.util.Date;
+
 
 public class CreateCampaignTestUsingPropertyFile 
 {
